a4aa8dd02188bf7a45789a87447b40d7afdca87f
Adebola Adeniran <onedebos@gmail.com>
2025-03-11 16:02:45 +0000

    update meta images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,32 @@ const jetbrains = JetBrains_Mono({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "SupportAgent",
   description:
-    "A Support Agent for a Shopify store using Alloy's Passthroguh APIs",
+    "A Support AI Agent for a Shopify store using Alloy's Passthroguh APIs",
+  metadataBase: new URL("https://runalloy.com"), // Replace with your actual domain
+  openGraph: {
+    title: "SupportAgent",
+    description:
+      "A Support AI Agent for a Shopify store using Alloy's Passthrough APIs",
+    images: [
+      {
+        url: "/alloy.png",
+        width: 1200,
+        height: 630,
+        alt: "SupportAgent for Shopify",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "SupportAgent",
+    description:
+      "A Support Agent for a Shopify store using Alloy's Passthrough APIs",
+    images: ["/alloy.png"],
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
